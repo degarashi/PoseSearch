178ff326fb8055880bd386c64005fe4c57937e91
Degarashi <degarasu@gmail.com>
2025-09-17 13:58:07 +0900

    [update] 新しいMasseTorsoVecテーブル定義

--- table_def.sql
+++ table_def.sql
@@ -47,15 +47,19 @@ CREATE TABLE Landmark (
 
 -- torsodir の計算方法と信頼度を格納するテーブル
 CREATE TABLE MasseTorsoDir (
-    poseId      INTEGER NOT NULL UNIQUE,
-    x           REAL NOT NULL,
-    y           REAL NOT NULL,
-    z           REAL NOT NULL,
-    method      TEXT NOT NULL,
-    score       REAL NOT NULL CHECK(score BETWEEN 0 AND 1),
-    embedded    BLOB NOT NULL CHECK(LENGTH(embedded) = 12),
-    FOREIGN KEY (poseId) REFERENCES Pose(id),
-    CHECK((x*x + y*y + z*z) BETWEEN 0.995 AND 1.005)
+	poseId      INTEGER PRIMARY KEY REFERENCES Pose(id),
+	x           REAL NOT NULL,
+	y           REAL NOT NULL,
+	z           REAL NOT NULL,
+	method      TEXT NOT NULL,                                  -- for debug (どのメソッドを使って算出したか)
+	score       REAL NOT NULL CHECK(score BETWEEN 0 AND 1),     -- どのくらい自信を持って言えるか
+	embedded    BLOB NOT NULL CHECK(LENGTH(embedded) == 4*3),   -- (x,y,z)をバイト列にした物。後の計算用(不要かも)
+	yaw_x       REAL NOT NULL,
+	yaw_z       REAL NOT NULL,
+	embedded_yaw BLOB NOT NULL CHECK(LENGTH(embedded_yaw) == 4*2),   -- (yaw_x,yaw_z)をバイト列にした物
+	pitch       REAL NOT NULL CHECK(pitch BETWEEN -1.0 AND 1.0),
+	CHECK((x*x + y*y + z*z) BETWEEN 0.995 AND 1.005)            -- should be unit vector
+	CHECK((yaw_x*yaw_x + yaw_z*yaw_z) BETWEEN 0.995 AND 1.005)  -- should be unit vector
 );
 
 -- 各ポーズの信頼度情報を格納するテーブル
@@ -68,8 +72,9 @@ CREATE TABLE Reliability (
 
 -- torsodir をベクトルとして扱うための仮想テーブル
 CREATE VIRTUAL TABLE MasseTorsoVec USING vec0(
-    poseId      INTEGER NOT NULL UNIQUE,
-    torsoDir    float[3]
+	poseId      INTEGER NOT NULL UNIQUE,
+	dir         float[3],
+	yaw         float[2]
 );
 
 CREATE TABLE TagInfo (
