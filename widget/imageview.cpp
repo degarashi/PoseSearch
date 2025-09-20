38505c0aef2e946daf77b79473e2334b1098efd7
Degarashi <degarasu@gmail.com>
2025-09-20 18:29:43 +0900

    [update] ImageView: リファクタリング

--- widget/imageview.cpp
+++ widget/imageview.cpp
@@ -1,14 +1,24 @@
 #include "imageview.h"
+#include <QPainter>
 
 ImageView::ImageView(QWidget *parent) : QWidget(parent) {
 }
 
 void ImageView::paintEvent(QPaintEvent *) {
 	QPainter painter(this);
-	painter.drawImage(rect(), image);
+	// 空画像の場合は描画しない
+	if (!image.isNull()) {
+		painter.drawImage(rect(), image);
+	}
 }
 
 void ImageView::setImage(const QImage &img) {
-	image = img;
+	if (!img.isNull()) {
+		// ウィジェットサイズに合わせてスケーリングして保持
+		image = img.scaled(size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+	}
+	else {
+		image = QImage();
+	}
 	update();
 }
