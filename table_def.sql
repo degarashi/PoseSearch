bc55d49da812b74d113e8f99155888c46ae70e6f
Degarashi <degarasu@gmail.com>
2025-10-02 14:56:57 +0900

    [add] 姿勢解析の不具合対応用blacklistテーブル追加
    姿勢解析が正確でないケースをユーザーが手動で特定、その情報を永続化する目的。
    `poseId`を格納する`blacklist.Blacklist`テーブルの新規定義。

--- table_def.sql
+++ table_def.sql
@@ -3,3 +3,8 @@ CREATE TABLE thumb.Thumbnail (
 	fileId		INTEGER PRIMARY KEY,
 	cacheName	TEXT NOT NULL UNIQUE
 );
+
+-- 姿勢解析が上手くいってないのをユーザーが手動でフラグ付けする --
+CREATE TABLE blacklist.Blacklist (
+	poseId		INTEGER PRIMARY KEY
+);
