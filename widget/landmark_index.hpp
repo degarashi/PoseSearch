361d5ddeda5d67c718d29f14750373b47d086cc8
Degarashi <degarasu@gmail.com>
2025-10-14 22:20:22 +0900

    [add] Cocoランドマーク定義の追加
    COCOデータセットのランドマークインデックスを定義

--- widget/landmark_index.hpp
+++ widget/landmark_index.hpp
@@ -36,3 +36,28 @@ enum class BlazePoseLandmarkIndex {
 	LEFT_FOOT_INDEX = 31,
 	RIGHT_FOOT_INDEX = 32
 };
+
+// Cocoのランドマーク定義
+enum class CocoLandmarkIndex {
+	NOSE = 0,
+	LEFT_EYE = 1,
+	RIGHT_EYE = 2,
+	LEFT_EAR = 3,
+	RIGHT_EAR = 4,
+	LEFT_SHOULDER = 5,
+	RIGHT_SHOULDER = 6,
+	LEFT_ELBOW = 7,
+	RIGHT_ELBOW = 8,
+	LEFT_WRIST = 9,
+	RIGHT_WRIST = 10,
+	LEFT_HIP = 11,
+	RIGHT_HIP = 12,
+	LEFT_KNEE = 13,
+	RIGHT_KNEE = 14,
+	LEFT_ANKLE = 15,
+	RIGHT_ANKLE = 16,
+	LEFT_HEEL = 17,
+	RIGHT_HEEL = 18,
+	LEFT_FOOT_INDEX = 19,
+	RIGHT_FOOT_INDEX = 20,
+};
